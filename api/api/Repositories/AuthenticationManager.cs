4fef12eaf00875a4ba06cdfe5d439c2f51fa4ca7
idiliev18 <idiliev18@codingburgas.bg>
2022-11-14 20:40:04 +0200

    Implement JWT Token generation

--- api/api/Repositories/AuthenticationManager.cs
+++ api/api/Repositories/AuthenticationManager.cs
@@ -60,9 +60,37 @@ namespace api.Repositories
             throw new NotImplementedException();
         }
 
-        private Task<string> GenerateToken()
+        private async Task<string> GenerateToken()
         {
-            throw new NotImplementedException();
+            var securityKey = new SymmetricSecurityKey(Encoding.
+                               UTF8.
+                               GetBytes(_configuration["JwtSettings:Key"]));
+
+            var credentials = new SigningCredentials(securityKey,
+                                   SecurityAlgorithms.HmacSha256);
+
+            var roles = await _userManager.GetRolesAsync(_user);
+            var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+            var userClaims = await _userManager.GetClaimsAsync(_user);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, _user.UserName),
+                new Claim("uid", _user.Id),
+            }
+            .Union(userClaims).Union(roleClaims);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.
+                         Now.
+                         AddMinutes(Convert.ToInt32(_configuration["JwtSettings:DurationInMinutes"])),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
